2144323aaae4b07f4928153a9f3832fda9c9d41f
pstierle <p.stierle@web.de>
2023-05-21 13:08:24 +0200

    init

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = 8080;
+const path = require("path");
+const distPath = path.join(__dirname, "dist");
 
-app.use(express.static(path.join(__dirname, 'dist', 'team-balancer-fe')))
+app.use(express.static(distPath));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'team-balancer-fe', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
-app.listen(port, '0.0.0.0');
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
