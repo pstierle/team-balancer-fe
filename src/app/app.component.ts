c1b15cc3fe21bdfbb510bedd4d9c1f2db39b4e50
pstierle <p.stierle@web.de>
2023-05-24 23:07:30 +0200

    sidenav over on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { LayoutService } from './core/services/layout.service';
+import {
+  AfterViewInit,
+  Component,
+  HostBinding,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { appRoutes } from './core/constants/app-routes.constant';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { BehaviorSubject } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSidenav)
+  public sideNav!: MatSidenav;
+
   @HostBinding('class')
   public theme = 'light-theme';
 
   public appRoutes = appRoutes;
   public sideNavOpen: boolean = false;
   public darkMode$ = new BehaviorSubject<boolean>(false);
+  public isSmallWindow$ = new BehaviorSubject<boolean>(false);
+  public showSideNavButton$ = this.layoutService.showSideNavButton$;
+  public sideNavMode$ = this.layoutService.sideNavMode$;
+
+  constructor(private layoutService: LayoutService) {}
 
   public ngOnInit(): void {
     const theme = localStorage.getItem('theme');
@@ -37,7 +54,15 @@ export class AppComponent implements OnInit {
     });
   }
 
+  public ngAfterViewInit(): void {
+    this.layoutService.init(this.sideNav);
+  }
+
   public handleDarkModeChange(event: MatSlideToggleChange): void {
     this.darkMode$.next(event.checked);
+  }
+
+  public showMenu(): void {
+    this.sideNav.open();
   }
 }
---
+++ src/app/core/services/layout.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LayoutService {
+  public showSideNavButton$ = new BehaviorSubject<boolean>(false);
+  public sideNavMode$ = new BehaviorSubject<MatDrawerMode>('over');
+  private sideNav!: MatSidenav;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  public init(sideNav: MatSidenav): void {
+    this.sideNav = sideNav;
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.Medium, Breakpoints.XSmall])
+      .subscribe((value) => {
+        if (value.matches) {
+          this.sideNav.close();
+          this.showSideNavButton$.next(true);
+          this.sideNavMode$.next('over');
+        } else {
+          this.sideNav.open();
+          this.showSideNavButton$.next(false);
+          this.sideNavMode$.next('side');
+        }
+      });
+  }
+}
